8e1ee6cd7ad5d79a65a1b3c6d94c2bdc65a440d2
CY <yesilkayacan@gmail.com>
2020-05-20 11:06:34 +0300

    docs: returuns.py module documentation
    Function and class level documentation done for returns.py module

--- time_series_analysis/returns.py
+++ time_series_analysis/returns.py
@@ -1,12 +1,29 @@
 import pandas as pd
 
 class Returns:
+    '''Class capable of processing returns data calculated from asset close prices.'''
+
     def __init__(self, close_dataframe):
+        '''Creates a Returns object capable of processing asset close data.
+        
+        Args
+        ----
+        close_dataframe : pandas.DataFrame
+            Dataframe where the asset close values are stored
+        '''
+
         self.data = close_dataframe
 
     
     def simple_net_returns(self):
-        '''Compute and return the daily return values.'''
+        '''Compute daily returns from the close data stored in self.
+        
+        Returns
+        -------
+        daily_returns : pandas.DataFrame
+            Daily returns data stored in a dataframe
+        '''
+
         daily_returns = self.data.copy()
         daily_returns.iloc[1:] = (daily_returns / daily_returns.shift(1)) - 1
         daily_returns.iloc[0,:] = 0
@@ -14,14 +31,24 @@ class Returns:
 
     
 class Distributions():
+    '''Class for working with normal distributions.'''
 
     def __init__(self, data):
-        self.returns_dataframe = data
-        self.means, self.stds, self.kurtosis = Distributions.calculate_distirbution_stats(self.returns_dataframe)
+        '''Creates distribution object and calculates the mean, standard deviation and kurtosis of the distribution.
+        These parameters can be accessed from the means, stds and kurtosis properties.
+
+        Args
+        ----
+        data : pandas.DataFrame
+            Data which the distribution parameters will be calculated on
+        '''
+
+        self.data = data
+        self.means, self.stds, self.kurtosis = Distributions.calculate_distirbution_stats(self.data)
 
 
     def calculate_distirbution_stats(data):
-        '''Calculates the mean, standard deviation and kurtosis of the input dataframe
+        '''Calculates the mean, standard deviation and kurtosis of the input dataframe.
         
         Args
         ----
